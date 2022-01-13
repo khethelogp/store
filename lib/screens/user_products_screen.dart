bc2e4d688c8aa239c1c55d59b6247cd2c1a1b655
Khethelo Ndhlala <khethelo.ndhlala@dadevs.co.za>
2022-01-13 17:00:07 +0200

    form validation

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,9 +1,14 @@
-/* import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
+import '../widgets/user_product_item.dart';
+import '../screens/edit_product_screen.dart';
+import '../widgets/app_drawer.dart';
 
 class UserProductsScreen extends StatelessWidget {
+
+  static const routeName = '/user-products';
 
   @override
   Widget build(BuildContext context) {
@@ -14,17 +19,28 @@ class UserProductsScreen extends StatelessWidget {
         actions: [
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {}, 
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            }, 
           )
         ]
       ),
+      drawer: AppDrawer(),
       body: Padding(
         padding: EdgeInsets.all(8),
         child: ListView.builder(
           itemCount: productsData.items.length,
-          itemBuilder: (_, i) =>  
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductItem(
+                productsData.items[i].title,
+                productsData.items[i].imageUrl
+              ),
+              Divider()
+            ],
+          )
         ),
       ),
     );
   }
-} */
+}
