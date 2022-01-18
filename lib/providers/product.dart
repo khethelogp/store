aeead861543f88f5603bc2ccbf056f695eacc517
Khethelo Ndhlala <khethelo.ndhlala@dadevs.co.za>
2022-01-18 16:52:33 +0200

    favorites

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +20,27 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  } 
+
+  void toggleFavoriteStatus() async{
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    var url = Uri.parse('https://flutter-store-cd51b-default-rtdb.firebaseio.com/products/$id.json');
+    try {
+      final response = await http.patch(url, body: json.encode({
+        'isFavoirte': isFavorite,
+        })
+      );
+      if(response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch(error) {
+      _setFavValue(oldStatus);
+    }
+
   }
 }
