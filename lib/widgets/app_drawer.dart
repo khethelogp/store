09970ad9c1153b18764289714bf6cc67b2052c5a
khethelogp <khethelogp@gmail.com>
2022-01-23 12:39:41 +0200

    Custom Animated Routes

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../screens/user_products_screen.dart';
 import '../screens/orders_screen.dart'; 
 import '../providers/auth.dart';
+import '../helpers/custom_route.dart';
 
 class AppDrawer extends StatelessWidget {
 
@@ -21,7 +22,12 @@ class AppDrawer extends StatelessWidget {
             leading: Icon(Icons.shop),
             title: Text('Shop'),
             onTap: (){
-              Navigator.of(context).pushReplacementNamed('/');
+              // Navigator.of(context).pushReplacementNamed('/');
+              Navigator.of(context).pushReplacement(
+                CustomRoute(
+                  builder: (ctx) => OrdersScreen(),
+                )
+              );
             },
           ),
           Divider(),
